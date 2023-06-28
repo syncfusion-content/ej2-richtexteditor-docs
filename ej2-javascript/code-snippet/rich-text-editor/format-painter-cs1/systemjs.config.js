a2f2456caf1a346a586d8ffac99ccb58f8923bd2
Gokulraj <gokulraj.devarjan@syncfusion.com>
2023-06-28 12:33:00 +0530

    Bug(835881): Solving Documentation issues for all platforms

--- ej2-javascript/code-snippet/rich-text-editor/format-painter-cs1/systemjs.config.js
+++ ej2-javascript/code-snippet/rich-text-editor/format-painter-cs1/systemjs.config.js
@@ -10,7 +10,7 @@ System.config({
         }
     },
     paths: {
-        "syncfusion:": "https://cdn.syncfusion.com/"
+        "syncfusion:": "https://cdn.syncfusion.com/ej2/"
     },
     map: {
         main: "index.ts",
